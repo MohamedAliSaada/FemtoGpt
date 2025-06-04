f3814c10a20d43845f4b249c4991a0eb3813663f
Mohamed Ali Saada <mohamedsaada2024@gmail.com>
2025-06-04 04:59:34 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="FemtoGpt",
     version="0.1",
-    description="A builder to GPT models from screatch",
-    author="Mohamed saada",
-    packages=find_packages(),
-    install_requires=["tensorflow>=2.11"],
+    description="A builder to GPT models from scratch",
+    author="Mohamed Saada",
+    packages=find_packages(),  # This will find FemtoGpt/ package
+    install_requires=[
+        "tensorflow>=2.11",
+    ],
 )
