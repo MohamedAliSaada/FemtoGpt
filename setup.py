a34391459086fce18e57443eeec7faec4c9a5ba1
Mohamed Ali Saada <mohamedsaada2024@gmail.com>
2025-06-05 08:31:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version="0.1",
     description="A builder to GPT models from scratch",
     author="Mohamed Saada",
-    packages=find_packages(),  # This will find FemtoGpt/ package
+    packages=find_packages(),
     install_requires=[
-        "tensorflow>=2.11",'sentencepiece'
+        "tensorflow>=2.11", "sentencepiece"
     ],
+    include_package_data=True,
 )
