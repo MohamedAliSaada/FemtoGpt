c4b359ac2d181dfa62439d26b5ad183c61d49d92
Mohamed Ali Saada <mohamedsaada2024@gmail.com>
2025-06-07 15:01:42 +0300

    Update __init__.py

--- FemtoGpt/__init__.py
+++ FemtoGpt/__init__.py
@@ -3,3 +3,4 @@ from .tokenizer import SaadaBPE
 from .Femto6000Tokenizer import Femto6000Tokenizer
 from .embedding import InputEmbedding
 from .outputhead import OutputHead
+from .femtogpt  import FemtoGPT
